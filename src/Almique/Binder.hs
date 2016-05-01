b4d1b8e5fc49bd7564263a747f888a71f4d426f3
Truls Asheim <truls@asheim.dk>
2016-05-01 05:28:21 +0200

    Can generate some VHDL now. Not done yet though...

--- src/Almique/Binder.hs
+++ src/Almique/Binder.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
---{-# LANGUAGE OverloadedRecordFields #-}
 
-module Almique.Binder () where
+module Almique.Binder
+       ( bindPyMod
+       , BindErr
+       ) where
 
 import Control.Monad.Reader
 import Control.Monad.Except
-import Data.Map.Strict hiding (map)
+import qualified Data.Map.Strict as Map hiding (map)
+import Data.Maybe (isNothing, fromMaybe)
+import Data.List
 
 import Language.SMEIL
 import qualified Almique.Analyzer as A
 
+type InstPorts = (Ident, ([Ident], [Ident]))
 type BindErr = String
 
 newtype BindM a = BindM { unBindM :: ExceptT BindErr (Reader A.AnState) a }
@@ -19,45 +24,219 @@ newtype BindM a = BindM { unBindM :: ExceptT BindErr (Reader A.AnState) a }
                        )
 
 runBindM :: A.AnState -> BindM Network -> Either BindErr Network
-runBindM s m = let
-  err = runReader (runExceptT $ unBindM m) s
-  in
-    case err of
-      Left e -> Left e
-      Right n -> Right n
+runBindM s m = runReader (runExceptT $ unBindM m) s
+
+-- | Query state of named function
+-- TODO: Maybe restate this in terms of withCurBlock
+queryNamedIS :: String -> (A.FunInterpState -> a) -> BindM a
+queryNamedIS i f = do
+  sts <- asks A.funStates
+  case Map.lookup i sts of
+    Just v -> return $ f v
+    Nothing -> throwError "Undefined function"
+
+queryFunIS :: String -> (Function -> a) -> BindM a
+queryFunIS s f = f <$> queryNamedIS s A.blockFunction
+
+-- TODO: This is basically the same function as in Analyzer: Find some way of generalizing
+queryIS :: (A.FunInterpState -> a) -> (a -> b) -> BindM b
+queryIS f t = do
+  hasCurBlock <- isNothing <$> asks A.currentBlock
+  when hasCurBlock $ throwError "Internal compiler error: Tried querying and empty block"
+  (Just curBlock) <- asks A.currentBlock
+  return $ t $ f curBlock
+
+queryFunCurIS :: (Function -> a) -> BindM a
+queryFunCurIS = queryIS A.blockFunction
+
+-- |Try to see if a free variable can be resolved. Very primitive at this point,
+-- only checks list of variables (At this point, we know that parameters of
+-- Complete functions becomes bound at runtime)
+resolveFree :: Variable -> BindM Bool
+resolveFree (NamedVar n) = queryIS A.params $ elem n
+resolveFree _ = throwError "resolveFree: Only NamedVar variables can be free?"
+
+-- |Lookup variable binding. If bound, return corresponding expression. If free,
+-- try resolving the binding as a parameter and return Nothing if unsuccessful
+lookupBinding :: Variable -> BindM (Maybe Expr)
+lookupBinding v = do
+  binding <- queryIS A.bindings (Map.lookup v)
+  case binding of
+    Just (A.Bound e) -> return $ Just e
+    Just (A.Free e@(Var var)) -> do
+      resolved <- resolveFree var
+      if resolved then
+        return $ Just e
+        else
+        return Nothing
+    _ -> return Nothing
+
+withCurBlock :: A.FunInterpState -> BindM a -> BindM a
+withCurBlock s = local (\c -> c { A.currentBlock = Just s })
+
+queryBusDef :: Ident -> (Bus -> a) -> BindM a
+queryBusDef i f = do
+  -- FIXME: Mostly a duplicate of queryNamedIS. Consolidate!
+  el <- asks A.busses
+  case Map.lookup i el of
+    Just v -> return $ f v
+    Nothing -> throwError $ "Reference to undefined bus" ++ i
+
+-- FIXME: Querying a bus just to see if queryBusDef will throw an
+-- error... Is this bad?
+checkBus :: Ident -> BindM Ident
+checkBus b = queryBusDef b id >> return b
 
 -- | For each function, check that all variables in function bodies are defined
 -- verify that variable kinds are correct. Furthermore, we should infer the
 -- the variable kinds being used
-bindFunction :: A.FunInterpState -> BindM Function
-bindFunction s = undefined
+bindFunction :: [InstPorts] -> BindM Function
+bindFunction ps = do
+  -- FIXME: Most of these mappings (and by extension the entire FunInterpState
+  -- type) ended up being rather pointless.
+  funName' <- queryFunCurIS funName
+  funType' <- queryFunCurIS funType
+  let (inPortTypes, outPortTypes) = getPortMap funName'
+  inPorts <- queryIS A.mappedInBusses (`zip` inPortTypes) -- >>= mapM checkBus
+  outPorts <- queryIS A.mappedOutBusses (`zip` outPortTypes) -- >>= mapM checkBus
+  funParams' <- map mapFunParam <$> queryIS A.params id -- >>= genFunParam
+  -- FIXME: Using toList feels like a bad idea
+  locals' <- (Map.toList <$> queryIS A.bindings id) >>= mapM mapBinding
+  funBody' <- queryFunCurIS funBody >>= mapStmts checkStmt
+  return Function { funName = funName'
+                  , funInports = inPorts
+                  , funOutports = outPorts
+                  , funParams = funParams'
+                  , locals = locals'
+                  , funBody = funBody'
+                  , funType = funType'
+                  }
+    where
+      mapFunParam :: Ident -> Decl
+      -- FIXME: Kind of useless right now. The Decl in order to enable future
+      -- support for default values
+      mapFunParam a = (Decl $ NamedVar a) Nothing
 
+      getPortMap :: Ident -> ([Ident], [Ident])
+      getPortMap i = fromMaybe ([], []) $ lookup i ps
+
+      mapStmts :: (Stmt -> BindM Stmt) -> Stmts -> BindM Stmts
+      mapStmts f (Stmts s)= Stmts <$> mapM f s
+
+      mapBinding :: (Variable, A.Binding) -> BindM Decl
+      mapBinding (i, _b) = do
+        binding <- lookupBinding i
+        case binding of
+          m@(Just _) -> return $ Decl i m
+          Nothing -> throwError $ "Reference to free variable " ++ show i
+
+-- Static checking of SMEIL
+-- TODO: (See TODO note below). Maybe output a list of variables modified by the
+  -- statement. Then, after all statements has been processed, match list of
+  -- bindings with list of touched variables and change variables that were
+  -- never touched to constants.
+checkStmt :: Stmt -> BindM Stmt
+checkStmt = return
+
+
+{- TODO:
+- Check variable bindings. If variable is resolved to a function
+  parameter, replace occurrences of the variable with the name of the
+  parameter. We do this, because parameters will be mapped to VHDL generics.
+- Infer variable constness. Variables that are never assigned to and have a default
+  value are inferred as constants. To do this, keep the kind of each variable
+  somewhere, maybe defaulting to constant and then change to regular variables
+  if assigned to or updated.
+-}
+checkVar :: Variable -> BindM Variable
+checkVar (ConstVar i) = throwError "Const vars are currently unsupported"
+checkVar (NamedVar i) = undefined
 
 -- | For each instance of Functions, check that the instantiation parameters
 -- causes all dependent variables of the function being instantiated to become
 -- bound variables. How to handle free variables in Externals is still unresolved.
 bindInstance :: A.NewInst -> BindM Instance
 bindInstance A.NewInst { A.instBindings = bindings
-                       , A.inst = i@Instance
-                         { params = ps } } = do
-  matchBindings $ zip bindings ps
-  return Instance {}
-  where
-    matchBindings :: [(A.Binding, Expr)] -> BindM ()
-    matchBindings = undefined
+                       , A.inst = Instance
+                         { instParams = _ps
+                         , instName = name
+                         , instFun = fun
+                         , inBusses = inb
+                         , outBusses = outb
+                         }
+                       } = do
+  -- Lookup the type of function referred to by instance
+  funtype <- queryFunIS name funType
+  -- Bindings of the function that we are instantiating
+  funBinds <- queryNamedIS name A.params
 
+  instBinds <- case funtype of
+        Complete -> do
+          unless (length funBinds == length bindings) $ throwError "Parameter count mismatch"
+          mapM mapBinding $ zip funBinds bindings
+        Skeleton ->
+          -- For non-synthesizable processes we simply create empty
+          -- "placeholder" bindings and we don't care about param length mismatches
+          return $ zip funBinds $ repeat EmptyVal
+        Undecided ->
+          throwError "Function of unknown kind encountered"
+
+  inb' <- mapM checkBus inb
+  outb' <- mapM checkBus outb
+  -- We do explicit mappings of type parameters here to trigger warnings if we
+  -- change the SMEIL AST
+  return Instance { instParams = instBinds
+                  , instName = name
+                  , instFun = fun
+                  , inBusses = inb'
+                  , outBusses = outb'
+                  }
+  where
+    -- Looks up binding and and maps it to function parameter if it is bound
+    mapBinding :: (Ident, A.Binding) -> BindM (Ident, PrimVal)
+    mapBinding (ident, A.Bound (Prim p)) = return (ident, p)
+    mapBinding (_, A.Bound _) = throwError "Variable bound to non-primitive value"
+    mapBinding _ = throwError "Instance parameter bound to free variable"
+
+genPortMap :: Instance -> InstPorts
+genPortMap Instance { instFun = fun
+                    , inBusses = ins
+                    , outBusses = outs
+                    } = (fun, (ins, outs))
+
+unifyPorts :: [InstPorts] -> BindM [InstPorts]
+-- FIXME: Is partial function (fst) usage safe here?
+unifyPorts ps = foldM merge [] (sortOn fst ps)
+  where
+    -- Its an error if the same function are instantiated more than once with
+    -- different busses
+    merge :: [InstPorts] -> InstPorts -> BindM [InstPorts]
+    merge ips@(ip@(i, (ob, ib)):_) ip'@(i', (ob', ib'))
+      | i == i'
+        && ((ob /= ob')
+        || (ib /= ib')) =  throwError $ unlines [ "Unable to unify ports in function instantiations"
+                                                , "Instantiation of " ++ i ++ " with ports " ++ show ob ++ show ib ++ "conflicts with previous instantiatino of " ++ i ++ " with ports " ++ show ob ++ show ib ]
+      | ip == ip' = return ips
+      | otherwise = return (ip':ips)
+    merge [] ip = return [ip]
 
 bindNetwork :: BindM Network
 bindNetwork = do
-  busList <- elems <$> asks A.busses
-  funs <- asks A.funStates
-  funs' <- mapM bindFunction funs
-  insts <- elems <$> asks A.instances
-  insts' <- mapM bindInstance insts
-  return Network { functions = funs'
+  busList <- Map.elems <$> asks A.busses
+  insts <- Map.elems <$> asks A.instances >>= mapM bindInstance
+  instPorts <- unifyPorts $ map genPortMap insts
+  funs <- Map.elems <$> asks A.funStates >>= mapM (`withCurBlock` bindFunction instPorts)
+  netName' <- asks A.netName
+  return Network { functions = funs
                  , busses = busList
-                 , instances = insts'
+                 , instances = insts
+                 , netName = netName'
                  }
 
 bindPyMod :: A.AnState -> Either BindErr Network
 bindPyMod s = runBindM s bindNetwork
+
+-- We wish to map function parameters to VHDL generics. In order to make sure
+-- that we have "safe passage" we need to check that all parameters of the
+-- processes will be made bound by the parameters given at the time of process
+-- instantiation.
